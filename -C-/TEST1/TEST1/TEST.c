485e55e8079b27f57ff28c11b617db949bedaa26
Akihi06 <2827843788@qq.com>
2024-07-08 22:37:16 +0800

    Add files via upload

---
+++ -C-/TEST1/TEST1/TEST.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	//printf("Hello,World!\n");
+	int a = 0;
+	scanf("%d", &a);
+	printf("a=%d\n", a);
+
+	return 0;
+}
+
+//newc++file.cpp

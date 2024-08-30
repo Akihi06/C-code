947341dafb2ec7283d29dd391ead5f4e142b6dc4
Akihi06 <2827843788@qq.com>
2024-08-30 17:54:06 +0800

    提交代码测试

---
+++ C-20240828/C-20240828/C-20240828.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num % 2 == 1);
+//	printf("%d\n", num);
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int age = 0;
+//	scanf("d", &age);
+//	if (age >= 18)
+//		printf("未成年人\n");
+//	else
+//		printf("成年人\n");
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
---
+++ C-20240828/enc_temp_folder/752d5f5c36c2d274ff19bb4c38d725b0/C-20240828.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num % 2 == 1);
+//	printf("%d\n", num);
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int age = 0;
+//	scanf("d", &age);
+//	if (age >= 18)
+//		printf("未成年人\n");
+//	else
+//		printf("成年人\n");
+//
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+	printf("%d\n", a);  // 10,10
+	printf("a");    //10,a
+	printf(a);     // 10 
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+

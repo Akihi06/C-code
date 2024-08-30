b52db2646ea1c26d7186843e93b3b4293813deea
Akihi06 <2827843788@qq.com>
2024-08-30 17:35:54 +0800

    C-code

---
+++ C-20240718/C-20240718/C-20240718.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ C-20240802/C-20240802/C-20240802.c
@@ -0,0 +1,148 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("µÃ·ÖÎª%d\n", a);
+//
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;   
+//	float x = 0.0;
+//	float y = 0.0;
+//	scanf("%d%d%f%f", &i, &j, &x, &y);
+//	printf("%d %d %f %f", i, j, x, y);
+//
+//	return 0;
+//}
+
+/*
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	float c = 0.0;
+	     //scanf("%d%d%f", &a, &b, &c);
+	int ret = scanf("%d%d%f", &a, &b, &c);
+	printf("ret = %d\n", ret);
+	printf("%d %d %f", a, b, c);
+	return 0;
+}
+
+
+
+*/
+/*
+
+#include<stdio.h>
+int main()
+{
+	char arr[11]="xxx";
+	//scanf("  %s", arr);
+	scanf("%s", arr);
+	printf("%s\n", arr);
+	return 0;
+}
+
+*/
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a = 0;
+//	scanf("%c", &a);
+//	printf("a=%c\n", a);
+//
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a = 0;
+//	scanf("%c", &a);
+//	printf("a=---%c---\n", a);
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a = 0;
+//	scanf(" %c", &a);
+//	printf("a=%c\n", a);
+//
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int year = 0;
+//	int month = 0;
+//	int day = 0;
+//	//scanf("%d%d%d", &year, &month, &day);
+//	int ret = scanf("%d%*c%d%*c%d", &year, &month, &day);
+//	printf("ret=%d\n", ret);
+//	printf("%d-%d-%d\n", year, month, day);
+//
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	printf("%d\n", strlen("c:\test\121"));
+//	printf(c:\test\121);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 7;
+//	int b = 2;
+//	float c = 2.0f;
+//	printf("%d\n", a / b);
+//	printf("%f\n", a / c);
+//
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d%d", &a, &b);
+	printf("%d %d", a / b, a % b);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
